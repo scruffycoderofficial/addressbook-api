b6e72f06afbcc98fe483834d2a8d0b98fd919e57
Luyanda Siko <sikoluyanda@gmail.com>
2021-02-03 23:51:45 +0200

    Added a Unit Test for ContactsController indexAction for listing all Contacts

---
+++ tests/unit/Contact/Business/Controller/ContactsControllerTest.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Class ContactsControllerTest
+ */
+class ContactsControllerTest extends TestCase
+{
+    /** 
+     * @test 
+     * @smoke
+     */
+    public function it_has_a_list_of_active_contacts()
+    {
+        $this->get('v1/api/contacts');
+
+        $this->seeJsonStructure(
+            [ 
+                'data' => [ 
+                    [
+                        'id', 
+                        'first_name', 
+                        'last_name', 
+                        'mobile_number', 
+                        'email_address'
+                    ]
+                ]
+            ]
+        );
+    }
+}
